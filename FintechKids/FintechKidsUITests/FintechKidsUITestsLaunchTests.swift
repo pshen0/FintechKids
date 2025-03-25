f35e56aed415d01ad4a84d87392a166c647cdc63
ProSkyMishka <mprozorskiy@mail.ru>
2025-03-25 23:43:39 +0300

    Fix xcode version conflict

--- FintechKids/FintechKidsUITests/FintechKidsUITestsLaunchTests.swift
+++ FintechKids/FintechKidsUITests/FintechKidsUITestsLaunchTests.swift
@@ -17,7 +17,6 @@ final class FintechKidsUITestsLaunchTests: XCTestCase {
         continueAfterFailure = false
     }
 
-    @MainActor
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
