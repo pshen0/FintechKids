c1156aa201eaaaf28b1248e04018879b01218972
Tagir Fayrushin <fayrushintagir@gmail.com>
2025-03-31 13:58:46 +0300

    adjustment llm

---
+++ FintechKids/FintechKids/Networking/LLMKeyProvider.swift
@@ -0,0 +1,12 @@
+//
+//  LLMKeyProvider.swift
+//  FintechKids
+//
+//  Created by Тагир Файрушин on 29.03.2025.
+//
+
+import Foundation
+
+enum LLMKeyProvider: String {
+    case apiKey = "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"
+}
